9afbe3b8d295d5423dfe9924eb9f27fc1a89bd3d
Eduardo Alves <eduardo4ms@gmail.com>
2023-04-10 13:31:36 -0300

    Revert "Barra lateral e gráficos"
    This reverts commit 293f473ac89c363fc520cb0dcb5f746324257226.

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -5,52 +5,17 @@ import { User  } from 'react-feather';
 import { Settings  } from 'react-feather';
 import { Star  } from 'react-feather';
 
-var open = true;
-
-function openbar(){
-  if(open==false){
-      let sidebar = document.getElementById("sidebar");
-      sidebar.style.width = "50px";
-      sidebar.style.textAlign = "center";
-      open = true;
-      let descriptions = document.getElementsByClassName("bar-text");
-      for (var i = 0; i < descriptions.length; i ++) {
-        descriptions[i].style.display = 'none';
-    }
-    return;
-  }
-  if(open==true){
-      let sidebar = document.getElementById("sidebar");
-      sidebar.style.width = "250px";
-      sidebar.style.textAlign = "left";
-      open = false;
-      let descriptions = document.getElementsByClassName("bar-text");
-      for (var i = 0; i < descriptions.length; i ++) {
-        descriptions[i].style.display = 'contents';
-    }
-    return;
-  }
-  }
-
 
 const Sidebar = () => (
   <>  
-    <nav id="sidebar">
+    <nav>
       <ul>
-        <li onClick={openbar}><Menu size={40}/>
-        <h5 class="bar-text">Menu</h5>
-        </li>
-        <li><Star size={40} />
-        <h5 class="bar-text">Pontuação</h5>
-        </li>
+        <li><Menu size={40} /></li>
+        <li><Star size={40} /></li>
       </ul>
       <ul>
-        <li><User size={40} />
-        <h5 class="bar-text">Perfil</h5>
-        </li>
-        <li><Settings size={40} />
-        <h5 class="bar-text">Configurações</h5>
-        </li>
+        <li><User size={40} /></li>
+        <li><Settings size={40} /></li>
       </ul>
     </nav>
   </>
